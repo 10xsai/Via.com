3ef45a447ae708ad04c239f2d79c3aef039db48a
krishnagottipalli <saikrishna.gdsv@gmail.com>
2021-08-12 14:29:05 +0530

    created Classes in login class and created base package

---
+++ src/test/java/base/Base.java
@@ -0,0 +1,33 @@
+package base;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class Base {
+	public WebDriver driver;
+	
+	@BeforeTest
+	public void beforeTest() {
+		Properties props = new Properties();
+		try {
+			props.load(new FileInputStream("src/test/resources/settings.property"));			
+		}
+		catch (Exception e) {
+			System.out.println(e);
+			driver.quit();
+		}
+		
+		System.setProperty("webdriver.chrome.driver", props.getProperty("cpath"));
+		ChromeDriver driver = new ChromeDriver();
+	}
+	
+	@AfterTest
+	public void afterTest() {
+		driver.quit();
+	}
+}
