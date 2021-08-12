58d83dff2ea9a6ebed89c095c01893976e2b49ac
krishnagottipalli <saikrishna.gdsv@gmail.com>
2021-08-12 10:08:53 +0530

    added settings.property and added drivers

--- src/test/java/Login/Test.java
+++ src/test/java/Login/Test.java
@@ -1,5 +1,30 @@
-package Login;
+package login;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		Properties props = new Properties();
+		props.load(new FileInputStream("src/test/resources/settings.property"));
+		
+		System.setProperty("webdriver.chrome.driver", props.getProperty("cpath"));
+		ChromeDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.selenium.dev");
+		
+		driver.findElement(By.id("gsc-i-id1")).sendKeys("LTI");
+		driver.findElement(By.linkText("Downloads")).click();
+		
+		System.out.println();
+		
+		Thread.sleep(2000);
+		driver.quit();
+	}
 
 }
