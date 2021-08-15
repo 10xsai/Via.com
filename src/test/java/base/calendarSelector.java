db963571dfa597f24efc169b7f97653287a4d2c2
Mir Taimur <66686127+mirtaimur@users.noreply.github.com>
2021-08-15 17:26:31 +0530

    Update calendarSelector.java

--- src/test/java/base/calendarSelector.java
+++ src/test/java/base/calendarSelector.java
@@ -13,23 +13,33 @@ import org.openqa.selenium.JavascriptExecutor;
 
 
 public class calendarSelector {
+	
+	// To declare the web driver
 	public WebDriver driver;
 	
+	// Constructor declaration
 	public calendarSelector(WebDriver driver) {
-		this.driver = driver;
+			this.driver = driver;
 	}
 	
 	public void selectDate(String date, Properties prop) throws InterruptedException {
-
+	
+	// Departure date
 	String[] depDt = date.split("-");
+	
+	// Departure Month
 	String dMonth = depDt[1];
 	
 	String dDate = depDt[0];
+		
+	// To click the calender icon on the website
 	driver.findElement(By.xpath(prop.getProperty("calendarIcon"))).click();	
 	Thread.sleep(4000);
 
+	// Declaring web element
 	WebElement deptEle = driver.findElement(By.xpath(prop.getProperty("departure")));
 
+	// Declaring explicit wait
 	WebDriverWait wt = new WebDriverWait(driver,30);
 	  wt.until(ExpectedConditions.visibilityOf(deptEle));
 	  wt.until(ExpectedConditions.elementToBeClickable(deptEle));
