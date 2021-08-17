974adfbde42a830e36fd1e0e915685e8b23d5711
sai krishna gottipalli <saikrishna.gdsv@gmail.com>
2021-08-17 08:57:34 +0530

    updated source files

--- src/test/java/login/RunnerLogin.java
+++ src/test/java/login/RunnerLogin.java
@@ -2,8 +2,14 @@ package login;
 
 import org.testng.annotations.Test;
 
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
+
 import base.Base;
 import base.ExcelFramework;
+
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
@@ -17,20 +23,32 @@ public class RunnerLogin extends Base {
 
 	@Test(dataProvider = "dp")
 	public void f(String email, String pwd) {
+		
+		ExtentReports ex = new ExtentReports(); // To Specify we are working with extent report
+		ex.attachReporter(new ExtentHtmlReporter("Login.html"));
+		ExtentTest tc=ex.createTest("LoginTest");
+
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 		driver.findElement(By.id("wzrk-cancel")).click();
-
 		driver.findElement(By.xpath(prop.getProperty("signin"))).click();
-
+		tc.pass("Cancelling popup");
 
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-		driver.findElement(By.xpath(prop.getProperty("emailid"))).sendKeys(email); // Please enter first name using letters
-																				// only.
+		driver.findElement(By.xpath(prop.getProperty("emailid"))).sendKeys(email); 
+		Assert.assertTrue(email.contains("@"), "Email id should contain '@'");
+		Assert.assertEquals(email,"mirtaimur7@gmail.com");
+		tc.pass("Checking Email field");
+
 		driver.findElement(By.xpath(prop.getProperty("password"))).sendKeys(pwd);
+		Assert.assertEquals(pwd, "taimur123");
+		tc.pass("Checking Password Field");
 
 		driver.findElement(By.xpath(prop.getProperty("loginbutton"))).click();
-
+		String hi = driver.findElement(By.xpath(prop.getProperty("loginSuccess"))).getText();
+		Assert.assertEquals(hi,"Hi Mir");
+		tc.pass("Checking Login button");
+		ex.flush();
 	}
 
 	
